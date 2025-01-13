0abee87f5dceba701fd8fbc3213360e9a135dcf8
chiaralle <chiaraallegrino@gmail.com>
2025-01-13 09:52:45 +0100

    add: commenti e modifiche

--- main.js
+++ main.js
@@ -10,18 +10,20 @@ Consigli del giorno:
 
 //creo un ciclo che sampi i numeri da 1 a 100
 /*creo un if e un else if
--per i numeri che sono multipli di 3 al posto del numero compare la scritta Fizz
--per i numeri che sono multipli di 5 comparirà invece la scritta Buzz
--per quelli che sono multipli sia di 3 che di 5 comparirà la scritta Fizzbuzz
+-per i numeri che sono multipli di 3 al posto del numero compare la scritta Fizz quindi vedere con il modulo % se è divisibile per 3
+-per i numeri che sono multipli di 5 comparirà invece la scritta Buzz quindi vedere con il modulo % se è divisibile per 5
+-per quelli che sono multipli sia di 3 che di 5 comparirà la scritta Fizzbuzz quindi divisibili per 3 per 5 
 */
 
 for (let i = 1; i <= 100; i++) {
-    if( i % 3 == 0 ) {
+     if ( i % 3 == 0 && i % 5 == 0) {
+      console.log(`FizzBuzz`);
+    } else if( i % 3 == 0 ) {
       console.log(`Fizz`);
     } else if ( i % 5 == 0 ) {
       console.log(`Buzz`);
     } else {
-      console.log(`FizzBuzz`);
+      console.log(`${i}`);
     }
     
   }
